858521726f66147cc6f10951eb650be25a05f77a
Nabby12 <Nabby12@users.noreply.github.com>
2021-01-20 07:02:03 +0900

    added/shaping-text-method-to-line-module

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,12 +1,50 @@
-/**
- * A Lambda function that returns a static string
- */
-exports.handler = async () => {
-    // If you change this message, you will need to change hello-from-lambda.test.js
-    const message = 'Hello from Lambda!';
+'use strict'
 
-    // All log statements are written to CloudWatch
-    console.info(`${message}`);
+const lineModule = require('./lineModule');
+const dropboxModule = require('./dropboxModule');
+
+exports.handler = async (event) => {
+    let result;
+    let replyStatus;
+
+    const lineAuthorize = await lineModule.authorize(event);
+    if (!lineAuthorize.isAuthorize) {
+        result = {'status': 'line authorize failed.'};
+        replyStatus = await lineModule.reply(event, result.status);
+        result.isReply = replyStatus.isReply;
+        return result;
+    };
+
+    const sendText = lineModule.shapingText(event);
+    const todayString = getTodayString();
+
+    // dropboxからinboxファイル内容取得（request-promise使おうかな）
+
+    // dropboxから取得した内容 + 格納した変数を合わせる
     
-    return message;
+    // dropboxのinboxファイルにテキストアップロード（上書き=overwrite）
+
+    // レスポンスをlineに返信
+}
+
+function getTodayString() {
+    const dayOfTheWeekArray = [
+        'Sun',
+        'Mon',
+        'Tue',
+        'Wed',
+        'Thu',
+        'Fri',
+        'Sat',
+    ]
+
+    const today = new Date();
+    const year = today.getFullYear().toString();
+    const month = ('00' + (today.getMonth() + 1)).slice(-2).toString();
+    const date = ('00' + today.getDate()).slice(-2).toString()
+    const dayOfTheWeek = dayOfTheWeekArray[today.getDay()];
+
+    const result = year + month + date + '_' + dayOfTheWeek;
+
+    return result;
 }
