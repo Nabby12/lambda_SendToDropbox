1657193aa5eb820aa6be15c09bb4654d5baee92b
Nabby12 <Nabby12@users.noreply.github.com>
2021-01-21 21:44:27 +0900

    modified/sample-test-code

--- __tests__/unit/handlers/index.test.js
+++ __tests__/unit/handlers/index.test.js
@@ -1,20 +1,15 @@
-// Import all functions from hello-from-lambda.js
-const lambda = require('../../../src/handlers/index.js');
+'use strict'
 
-// This includes all tests for helloFromLambdaHandler()
-describe('Test for hello-from-lambda', function () {
-    // This test invokes helloFromLambdaHandler() and compare the result 
-    it('Verifies successful response', async () => {
-        // Invoke helloFromLambdaHandler()
-        const result = await lambda.helloFromLambdaHandler();
-        /* 
-            The expected result should match the return from your Lambda function.
-            e.g. 
-            if you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in hello-from-lambda.js
-            you should change the following line to `const expectedResult = 'Hello World!';`
-        */
-        const expectedResult = 'Hello from Lambda!';
-        // Compare the result with the expected result
-        expect(result).toEqual(expectedResult);
+const sinon = require('sinon');
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const assert = chai.assert;
+const expect = chai.expect;
+
+const index = require('../../../src/handlers/index.js');
+
+describe('Test for index', () => {
+    it('line認証に失敗した場合、認証失敗ステータスが返る', async () => {
+
     });
 });
