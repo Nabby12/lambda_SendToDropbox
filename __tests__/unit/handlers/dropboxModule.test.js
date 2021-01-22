7af19dd87e455b8ac4089736a48226c5bc67ecad
Nabby12 <Nabby12@users.noreply.github.com>
2021-01-23 00:48:54 +0900

    added/test-code-for-dropbox-module

---
+++ __tests__/unit/handlers/dropboxModule.test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const sinon = require('sinon');
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const assert = chai.assert;
+const expect = chai.expect;
+
+describe('Test for index', () => {
+    it('テキスト送信に失敗した場合、送信失敗ステータスが返る', async () => {
+
+    });
+
+    it('テキスト送信に成功した場合、送信成功ステータスが返る', async () => {
+
+    });
+});
